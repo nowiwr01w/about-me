f5616fbdf35a004be78e39caa516c19a9ecebe47
Andrey Larionov <nowiwr01p@icloud.com>
2024-09-22 01:10:15 +0400

    Description + remove unused fonts

--- app/core-ui/src/wasmJsMain/kotlin/com/nowiwr01p/me/core_ui/theme/Fonts.kt
+++ app/core-ui/src/wasmJsMain/kotlin/com/nowiwr01p/me/core_ui/theme/Fonts.kt
@@ -2,69 +2,14 @@ package com.nowiwr01p.me.core_ui.theme
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import com.nowiwr01p.me.resources.JetBrainsMonoNL_Bold
-import com.nowiwr01p.me.resources.JetBrainsMonoNL_BoldItalic
-import com.nowiwr01p.me.resources.JetBrainsMonoNL_ExtraBold
-import com.nowiwr01p.me.resources.JetBrainsMonoNL_ExtraBoldItalic
-import com.nowiwr01p.me.resources.JetBrainsMonoNL_Italic
-import com.nowiwr01p.me.resources.JetBrainsMonoNL_Light
-import com.nowiwr01p.me.resources.JetBrainsMonoNL_LightItalic
-import com.nowiwr01p.me.resources.JetBrainsMonoNL_Medium
-import com.nowiwr01p.me.resources.JetBrainsMonoNL_MediumItalic
-import com.nowiwr01p.me.resources.JetBrainsMonoNL_Regular
-import com.nowiwr01p.me.resources.JetBrainsMonoNL_SemiBold
-import com.nowiwr01p.me.resources.JetBrainsMonoNL_SemiBoldItalic
-import com.nowiwr01p.me.resources.JetBrainsMonoNL_Thin
-import com.nowiwr01p.me.resources.JetBrainsMonoNL_ThinItalic
-import com.nowiwr01p.me.resources.JetBrainsMono_Bold
-import com.nowiwr01p.me.resources.JetBrainsMono_BoldItalic
-import com.nowiwr01p.me.resources.JetBrainsMono_ExtraBold
-import com.nowiwr01p.me.resources.JetBrainsMono_ExtraBoldItalic
-import com.nowiwr01p.me.resources.JetBrainsMono_Italic
-import com.nowiwr01p.me.resources.JetBrainsMono_Light
-import com.nowiwr01p.me.resources.JetBrainsMono_LightItalic
-import com.nowiwr01p.me.resources.JetBrainsMono_Medium
-import com.nowiwr01p.me.resources.JetBrainsMono_MediumItalic
-import com.nowiwr01p.me.resources.JetBrainsMono_Regular
-import com.nowiwr01p.me.resources.JetBrainsMono_SemiBold
-import com.nowiwr01p.me.resources.JetBrainsMono_SemiBoldItalic
-import com.nowiwr01p.me.resources.JetBrainsMono_Thin
-import com.nowiwr01p.me.resources.JetBrainsMono_ThinItalic
-import com.nowiwr01p.me.resources.Res
+import com.nowiwr01p.me.resources.*
 import org.jetbrains.compose.resources.Font
 
 val JetBrainsMono @Composable get() = FontFamily(
     Font(Res.font.JetBrainsMono_Regular, FontWeight.Normal),
     Font(Res.font.JetBrainsMono_Bold, FontWeight.Bold),
     Font(Res.font.JetBrainsMono_ExtraBold, FontWeight.ExtraBold),
-    Font(Res.font.JetBrainsMono_Light, FontWeight.Light),
     Font(Res.font.JetBrainsMono_Medium, FontWeight.Medium),
     Font(Res.font.JetBrainsMono_SemiBold, FontWeight.SemiBold),
-    Font(Res.font.JetBrainsMono_Thin, FontWeight.Thin),
-    Font(Res.font.JetBrainsMono_Italic, FontWeight.Normal, FontStyle.Italic),
-    Font(Res.font.JetBrainsMono_BoldItalic, FontWeight.Bold, FontStyle.Italic),
-    Font(Res.font.JetBrainsMono_ExtraBoldItalic, FontWeight.ExtraBold, FontStyle.Italic),
-    Font(Res.font.JetBrainsMono_LightItalic, FontWeight.Light, FontStyle.Italic),
-    Font(Res.font.JetBrainsMono_MediumItalic, FontWeight.Medium, FontStyle.Italic),
-    Font(Res.font.JetBrainsMono_SemiBoldItalic, FontWeight.SemiBold, FontStyle.Italic),
-    Font(Res.font.JetBrainsMono_ThinItalic, FontWeight.Thin, FontStyle.Italic)
-)
-
-val JetBrainsMonoNL @Composable get() = FontFamily(
-    Font(Res.font.JetBrainsMonoNL_Regular, FontWeight.Normal),
-    Font(Res.font.JetBrainsMonoNL_Bold, FontWeight.Bold),
-    Font(Res.font.JetBrainsMonoNL_ExtraBold, FontWeight.ExtraBold),
-    Font(Res.font.JetBrainsMonoNL_Light, FontWeight.Light),
-    Font(Res.font.JetBrainsMonoNL_Medium, FontWeight.Medium),
-    Font(Res.font.JetBrainsMonoNL_SemiBold, FontWeight.SemiBold),
-    Font(Res.font.JetBrainsMonoNL_Thin, FontWeight.Thin),
-    Font(Res.font.JetBrainsMonoNL_Italic, FontWeight.Normal, FontStyle.Italic),
-    Font(Res.font.JetBrainsMonoNL_BoldItalic, FontWeight.Bold, FontStyle.Italic),
-    Font(Res.font.JetBrainsMonoNL_ExtraBoldItalic, FontWeight.ExtraBold, FontStyle.Italic),
-    Font(Res.font.JetBrainsMonoNL_LightItalic, FontWeight.Light, FontStyle.Italic),
-    Font(Res.font.JetBrainsMonoNL_MediumItalic, FontWeight.Medium, FontStyle.Italic),
-    Font(Res.font.JetBrainsMonoNL_SemiBoldItalic, FontWeight.SemiBold, FontStyle.Italic),
-    Font(Res.font.JetBrainsMonoNL_ThinItalic, FontWeight.Thin, FontStyle.Italic)
 )
--- app/src/wasmJsMain/kotlin/com/nowiwr01p/me/ui/HomeMainScreen.kt
+++ app/src/wasmJsMain/kotlin/com/nowiwr01p/me/ui/HomeMainScreen.kt
@@ -1,17 +1,25 @@
 package com.nowiwr01p.me.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.nowiwr01p.me.core_ui.theme.AppTypography
+import com.nowiwr01p.me.core_ui.theme.colorAccent
 import com.nowiwr01p.me.core_ui.theme.colorBackground
+import com.nowiwr01p.me.core_ui.theme.colorText
 import com.nowiwr01p.me.shared.ContactData
 import com.nowiwr01p.me.ui.HomeContract.*
-import com.nowiwr01p.me.ui.divider.Divider
-import com.nowiwr01p.me.ui.header.HeaderWithPhotoAndContacts
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import rememberViewModel
 
@@ -57,9 +65,123 @@ private fun Content(
             state = state,
             listener = listener
         )
-        Divider(topPadding = 32.dp)
+        Divider()
+        Description()
+        Divider()
     }
 }
+
+@Composable
+internal fun HeaderWithPhotoAndContacts(
+    state: State,
+    listener: Listener?
+) {
+    Contacts(
+        state = state,
+        listener = listener
+    )
+    NamePosition()
+    LocationTimezone()
+}
+
+/**
+ * CONTACTS ROW
+ */
+@Composable
+private fun Contacts(
+    state: State,
+    listener: Listener?
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .padding(vertical = 14.dp)
+            .fillMaxWidth()
+    ) {
+        state.contacts.forEach { contact ->
+            ContactItem(
+                contact = contact,
+                onContactClick = { listener?.onContactClick(contact) }
+            )
+        }
+    }
+}
+
+@Composable
+private fun ContactItem(
+    contact: ContactData,
+    onContactClick: () -> Unit
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .clip(RoundedCornerShape(16.dp))
+            .clickable { onContactClick() }
+    ) {
+        Text(
+            text = stringResource(contact.name).uppercase(),
+            color = colorText,
+            style = AppTypography.h4,
+            modifier = Modifier.padding(vertical = 10.dp, horizontal = 18.dp)
+        )
+    }
+}
+
+/**
+ * NAME AND POSITION
+ */
+ @Composable
+private fun NamePosition() {
+    Text(
+        text = "Andrey Larionov | Android Developer",
+        color = colorAccent,
+        style = AppTypography.h1
+    )
+}
+
+/**
+ * LOCATION AND TIMEZONE INFO
+ */
+ @Composable
+private fun LocationTimezone() {
+    Text(
+        text = "UTC+4 | Georgia, Tbilisi | GMT+4",
+        color = colorText,
+        style = AppTypography.h5,
+        modifier = Modifier.padding(top = 24.dp)
+    )
+}
+
+/**
+ * DESCRIPTION
+ */
+@Composable
+private fun Description() {
+    Text(
+        text = """
+        Hi, I’m an Android Developer with 6 years of experience and a degree in Computer Science. I build modern, efficient apps using tools like Jetpack Compose, Coroutines, DataStore, and WorkManager.
+        I’m currently exploring KMM to create cross-platform apps for Android, iOS, Desktop, and Web. I can also set up a Ktor server, integrate it with Postgres, and deploy it to hosting.
+        I’m passionate about the TON ecosystem and am interested in writing smart contracts as part of my exploration in blockchain development.
+        """.trimIndent(),
+        color = colorText,
+        style = AppTypography.h3.copy(lineHeight = 32.sp),
+        modifier = Modifier.padding(top = 24.dp)
+    )
+}
+
+/**
+ * HORIZONTAL DIVIDER
+ */
+@Composable
+internal fun Divider() = Box(
+    modifier = Modifier
+        .padding(top = 32.dp)
+        .fillMaxWidth()
+        .clip(RoundedCornerShape(16.dp))
+        .height(1.dp)
+        .background(colorText.copy(alpha = 0.2f))
+)
 
 /**
  * PREVIEW
